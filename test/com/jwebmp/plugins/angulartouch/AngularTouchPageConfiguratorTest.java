687c60c39c37abccdeaebf1aa7e13438e91bcb8a
GedMarc <ged_marc@hotmail.com>
2018-04-22 15:47:40 +0200

    Prepping for RC1 - Update Base Package Name to new Name

--- test/com/jwebmp/plugins/angulartouch/AngularTouchPageConfiguratorTest.java
+++ test/com/jwebmp/plugins/angulartouch/AngularTouchPageConfiguratorTest.java
@@ -1,6 +1,5 @@
 package com.jwebmp.plugins.angulartouch;
 
-import com.jwebmp.BaseTestClass;
 import com.jwebmp.Page;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +7,7 @@ import org.junit.jupiter.api.Test;
  * @author Marc Magon
  */
 public class AngularTouchPageConfiguratorTest
-		extends BaseTestClass
+
 {
 
 	public AngularTouchPageConfiguratorTest()
@@ -18,7 +17,7 @@ public class AngularTouchPageConfiguratorTest
 	@Test
 	public void testSomeMethod()
 	{
-		Page p = getInstance();
+		Page p = new Page();
 		System.out.println(p.toString(0));
 	}
 
