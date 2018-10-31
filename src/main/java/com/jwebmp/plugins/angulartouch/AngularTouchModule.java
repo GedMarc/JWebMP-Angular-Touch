0fd63d4183f5f70442e1ff344c5b7bbde18eac16
GedMarc <ged_marc@hotmail.com>
2018-10-31 12:54:34 +0200

    Remove Serializable by default, Update module-info to utilize transitive dependencies

--- src/main/java/com/jwebmp/plugins/angulartouch/AngularTouchModule.java
+++ src/main/java/com/jwebmp/plugins/angulartouch/AngularTouchModule.java
@@ -31,7 +31,6 @@ public class AngularTouchModule
 		implements IAngularModule<AngularTouchModule>
 {
 
-	private static final long serialVersionUID = 1L;
 
 	/*
 	 * Constructs a new AngularTouchModule
