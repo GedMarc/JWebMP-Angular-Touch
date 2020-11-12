f61045d9ccfb63d8911af731169a1fceea5ab670
GedMarc <ged_marc@hotmail.com>
2020-11-13 00:39:32 +0200

    module name java.validation to jakarta.validation

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -2,7 +2,7 @@ module com.jwebmp.plugins.angulartouch {
 	exports com.jwebmp.plugins.angulartouch;
 
 	requires com.jwebmp.core;
-	requires java.validation;
+	requires jakarta.validation;
 	requires com.guicedee.guicedinjection;
 	requires com.jwebmp.core.angularjs;
 
